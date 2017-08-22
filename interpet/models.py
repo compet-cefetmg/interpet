76ec475ca372d8733d7e589c54266421ab447053
Gustavo Ceconelli <guceconelli@hotmail.com>
2017-08-22 19:12:02 -0300

    Adapta formulários para o formulário pedido

--- interpet/models.py
+++ interpet/models.py
@@ -3,35 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Inscricao(models.Model):
-	Q1 = models.CharField(max_length=1000)
-	Q2 = models.CharField(max_length=1000, blank=True, null=True)
-	Q3 = models.CharField(max_length=1000, blank=True, null=True)
-	Q4 = models.CharField(max_length=1000, blank=True, null=True)
-	Q5 = models.CharField(max_length=1000, blank=True, null=True)
-	Q6 = models.CharField(max_length=1000, blank=True, null=True)
-	Q7 = models.CharField(max_length=1000, blank=True, null=True)
-	Q8 = models.CharField(max_length=1000, blank=True, null=True)
-	Q9 = models.CharField(max_length=1000, blank=True, null=True)
-	Q10 = models.CharField(max_length=1000, blank=True, null=True)
-	Q11 = models.CharField(max_length=1000, blank=True, null=True)
-	Q12 = models.CharField(max_length=1000, blank=True, null=True)
-	Q13 = models.CharField(max_length=1000, blank=True, null=True)
-	Q14 = models.CharField(max_length=1000, blank=True, null=True)
-	Q15 = models.CharField(max_length=1000, blank=True, null=True)
-	Q16 = models.CharField(max_length=1000, blank=True, null=True)
-	Q17 = models.CharField(max_length=1000, blank=True, null=True)
-	Q18 = models.CharField(max_length=1000, blank=True, null=True)
-	Q19 = models.CharField(max_length=1000, blank=True, null=True)
-	Q20 = models.CharField(max_length=1000, blank=True, null=True)
-
-	# Ouvinte
-	Q21 = models.CharField(max_length=1000, blank=True, null=True)
-	Q22 = models.CharField(max_length=1000, blank=True, null=True)
-	Q23 = models.CharField(max_length=1000, blank=True, null=True)
-
+	Q1 = models.CharField(max_length=1000,verbose_name='Nome')
+	Q2 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Sexo')
+	Q3 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='CPF')
+	Q4 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Telefone')
+	Q5 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='E-mail')
+	Q6 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Matricula')
+	Q7 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='PET')
+	Q8 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Instituicao')
+	Q9 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Curso')
+	Q10 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Estadia')
+	Q11 = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Alimentação')
 	ouvinte = models.BooleanField(default=False)
 	data = models.DateTimeField(auto_now=True, editable=True)
 
 	def __str__(self):
 		return self.Q1 + " - " + self.data.strftime("%H:%M %d/%m/%Y")
 
+	class Meta:
+		verbose_name = 'Inscrito'
+		verbose_name_plural = 'Inscritos'
--- interpet/static/interpet/assets/js/inscritos.js
+++ interpet/static/interpet/assets/js/inscritos.js
@@ -4,16 +4,40 @@ function format ( d ) {
     if(d.fields.ouvinte){
         return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
             '<tr>'+
-                '<th>Situação Acadêmica</th>'+
-                '<td>' + (d.fields.Q21 != null ? d.fields.Q21 : "") + '</td>'+
+                '<th width="30%">Sexo</th>'+
+                '<td>'+ (d.fields.Q2 != null ? d.fields.Q2 : "") +'</td>'+
             '</tr>'+
             '<tr>'+
-                '<th>Curso que atua</th>'+
-                '<td>' + (d.fields.Q22 != null ? d.fields.Q22 : "") + '</td>'+
+               '<th>CPF</th>'+
+                '<td>'+ (d.fields.Q3 != null ? d.fields.Q3 : "") +'</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Telefone</th>'+
+                '<td>'+ (d.fields.Q4 != null ?  d.fields.Q4 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>E-mail</th>'+
+                '<td>' + (d.fields.Q5 != null ? d.fields.Q5 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Matrícula</th>'+
+                '<td>' + (d.fields.Q6 != null ? d.fields.Q6 : "") + '</td>'+
             '</tr>'+
             '<tr>'+
                 '<th>Instituição de origem</th>'+
-                '<td>' + (d.fields.Q23 != null ? d.fields.Q23 : "") + '</td>'+
+                '<td>' + (d.fields.Q8 != null ? d.fields.Q8 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Curso</th>'+
+                '<td>' + (d.fields.Q9 != null ? d.fields.Q9 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Estadia</th>'+
+                '<td>' + (d.fields.Q10 != null ? d.fields.Q10 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Alimentação</th>'+
+                '<td>' + (d.fields.Q11 != null ? d.fields.Q11 : "") + '</td>'+
             '</tr>'+
         '</table>';
     }
@@ -21,76 +45,34 @@ function format ( d ) {
 
     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
         '<tr>'+
-            '<th width="30%">Idade</th>'+
-            '<td>'+ (d.fields.Q2 != null ? d.fields.Q2 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Egresso de escola</th>'+
-            '<td>'+ (d.fields.Q3 != null ? d.fields.Q3 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Atividades Acadêmicas</th>'+
-            '<td>'+ eval(d.fields.Q4).reduce(function(previousValue, currentValue){ return previousValue +  currentValue + ", "; }, "") +
-            		(d.fields.Q5 ? ", " + d.fields.Q5 : "") +
-            '</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Vínculo</th>'+
-            '<td>' + (d.fields.Q7 != null ? d.fields.Q7 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Por que ingressou no PET?</th>'+
-            '<td>' + (d.fields.Q8 != null ? d.fields.Q8 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Carga horária diária</th>'+
-            '<td>' + (d.fields.Q9 != null ? d.fields.Q9 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Onde desenvolve a maior parte das atividades do PET?</th>'+
-            '<td>' + (d.fields.Q10 != null ? d.fields.Q10 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Qual é a sua rotina semanal de contato com seu tutor?</th>'+
-            '<td>' + (d.fields.Q11 != null ? d.fields.Q11 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Qual é o meio mais utilizado para o contato com seu tutor?</th>'+
-            '<td>' + (d.fields.Q12 != null ? d.fields.Q12 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>O valor da bolsa é adequado?</th>'+
-            '<td>' + (d.fields.Q13 != null ? d.fields.Q13 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>O recebimento da bolsa colabora para a sua permanência no CEFET-MG?</th>'+
-            '<td>' + (d.fields.Q14 != null ? d.fields.Q14 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Das atividades desenvolvidas pelo grupo PET que você participa, com quais delas que você mais se identifica?</th>'+
-            '<td>' + (d.fields.Q15 != null ? d.fields.Q15 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th colspan="2">Para cada interface do PET, cite uma atividade desenvolvida pelo grupo que você participa:</th>'+
-        '</tr>'+
-        '<tr>'+
-            '<td align="right">Ensino</td>'+
-            '<td>' + (d.fields.Q16 != null ? d.fields.Q16 : "") +'</td>'+
-        '</tr>'+
-            '<td align="right">Pesquisa</td>'+
-            '<td>' + (d.fields.Q17 != null ? d.fields.Q17 : "") +'</td>'+
-        '</tr>'+
-            '<td align="right">Extensão</td>'+
-            '<td>' + (d.fields.Q18 != null ? d.fields.Q18 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>De que forma o PET pode colaborar para o mundo do trabalho e para a formação acadêmica?</th>'+
-            '<td>' + (d.fields.Q19 != null ? d.fields.Q19 : "") +'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<th>Você pretende seguir carreira acadêmica?</th>'+
-            '<td>' + (d.fields.Q20 != null ? d.fields.Q20 : "") +'</td>'+
-        '</tr>'+
+            '<th width="30%">Sexo</th>'+
+                '<td>'+ (d.fields.Q2 != null ? d.fields.Q2 : "") +'</td>'+
+            '</tr>'+
+            '<tr>'+
+               '<th>CPF</th>'+
+                '<td>'+ (d.fields.Q3 != null ? d.fields.Q3 : "") +'</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Telefone</th>'+
+                '<td>'+ (d.fields.Q4 != null ?  d.fields.Q4 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>E-mail</th>'+
+                '<td>' + (d.fields.Q5 != null ? d.fields.Q5 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Matrícula</th>'+
+                '<td>' + (d.fields.Q6 != null ? d.fields.Q6 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Estadia</th>'+
+                '<td>' + (d.fields.Q10 != null ? d.fields.Q10 : "") + '</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<th>Alimentação</th>'+
+                '<td>' + (d.fields.Q11 != null ? d.fields.Q11 : "") + '</td>'+
+            '</tr>'+
+        '</table>';
     '</table>';
 }
  
@@ -110,7 +92,7 @@ $(document).ready(function() {
                 "defaultContent": ''
             },
             { "data": "fields.Q1" },
-            { "data": "fields.Q6" },
+            { "data": "fields.Q7" },
         ],
         "order": [[1, 'asc']]
     } );
